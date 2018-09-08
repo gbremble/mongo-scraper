2fb31b54056827f8d0d35abc934407c02298a4d7
Greg Bremble <32407913+gbremble@users.noreply.github.com>
2018-09-08 08:06:57 -0400

    added html routes

---
+++ routes/html-routes.js
@@ -0,0 +1,16 @@
+const db = require('../models');
+
+module.exports = (app) => {
+    // GET route for retrieving all Articles from the database
+    app.get('/', (req, res) => {
+        // get every document in the Articles collection
+        db.Article.find({})
+            .then((dbArticle) => {
+                // if successful, send Articles back to the client
+                res.render('index', {
+                    articles: dbArticle,
+                });
+            })
+            .catch(error => res.json(error));
+    });
+};
